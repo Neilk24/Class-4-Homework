eef6969b4a3dbcfb78dfb422e2264e458e128bbb
Neilk24 <neilk2050@gmail.com>
2024-09-03 15:30:51 -0400

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the temperature outside: "))
+
+if 60<x<90:
+    print("It is okay to wear light clothes right now!")
+
+else:
+    print("It would not be great to wear light clothes right now.")
